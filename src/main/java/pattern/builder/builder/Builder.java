68f1282262abc587779ae90a1b2270356b62bfe3
firebat <powerfirebat@gmail.com>
2018-07-04 00:08:53 +0900

    builder, prototype 패턴 추가

---
+++ src/main/java/pattern/builder/builder/Builder.java
@@ -0,0 +1,18 @@
+package pattern.builder.builder;
+
+import pattern.builder.product.Product;
+
+import java.util.Date;
+
+/**
+ * Created by LeeJangHyeop on 2018. 7. 3..
+ */
+public interface Builder {
+    void setName(String name);
+    void setDescription(String description);
+    void setUnit(String unit);
+    void setPrice(Long price);
+    void setProductType(String productType);
+    void setCreatedDate(Date createdDate);
+    Product build();
+}
