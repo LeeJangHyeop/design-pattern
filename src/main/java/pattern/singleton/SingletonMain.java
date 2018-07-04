ea0f4855da301ce9efc31bc9d5b32dc1699d633f
firebat <powerfirebat@gmail.com>
2018-07-05 00:17:13 +0900

    singleton 패턴 추가

---
+++ src/main/java/pattern/singleton/SingletonMain.java
@@ -0,0 +1,17 @@
+package pattern.singleton;
+
+/**
+ * Created by LeeJangHyeop on 2018. 7. 5..
+ */
+public class SingletonMain {
+    public static void main(String[] args) {
+        MemoryStorage memoryStorage = MemoryStorage.getInstance();
+        memoryStorage.getStorage().put("name", "mason");
+
+        MemoryStorage memoryStorage2 = MemoryStorage.getInstance();
+        String name = memoryStorage2.getStorage().get("name");
+
+        System.out.println("memoryStorage == memoryStorage2 : " + (memoryStorage == memoryStorage2));
+        System.out.print("name : " + name);
+    }
+}
